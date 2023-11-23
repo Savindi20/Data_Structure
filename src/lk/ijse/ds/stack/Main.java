ee53d91a7207f9b4563bce65f87438b0fe669016
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-11-23 09:46:20 +0530

    Initial commit

---
+++ src/lk/ijse/ds/stack/Main.java
@@ -0,0 +1,20 @@
+package lk.ijse.ds.stack;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        stack.printStack();
+
+        System.out.println("Popped: " + stack.pop());
+        stack.printStack();
+
+        System.out.println("Popped: " + stack.pop());
+        stack.printStack();
+    }
+}
