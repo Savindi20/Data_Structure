a667ec14df9c267d98b78aa3a781c032d44d9e51
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-11-23 10:59:49 +0530

    queue classes addee

---
+++ src/lk/ijse/ds/queue/Main.java
@@ -0,0 +1,13 @@
+package lk.ijse.ds.queue;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue queue = new Queue(5);
+
+        queue.enQueue(10);
+        queue.enQueue(20);
+        queue.enQueue(30);
+
+        queue.printQueue();
+    }
+}
